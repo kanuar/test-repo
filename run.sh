d3c447d7b5a5dab16123277ee43c942efd4d9b7c
raunak kodwani <68312266+kanuar@users.noreply.github.com>
2024-08-11 22:49:57 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -L https://github.com/mortbopet/Ripes/releases/download/v2.2.5/Ripes-v2.2.5-linux-x86_64.AppImage --output ripes.AppImage
+chmod a+x ripes.AppImage
+
 # Initialize the pass counter
 pass_counter=0
 
